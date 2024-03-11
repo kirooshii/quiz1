5b9ba69ff36cca8ecdfc962a974ea0c9bf4dc28d
Makar Lavrov <120464003+kirooshii@users.noreply.github.com>
2024-03-11 07:33:19 +0000

    solution 1

--- Assignment01.java
+++ Assignment01.java
@@ -1,16 +1,16 @@
-package quizes.q1.makar_lavrov_1;
-
+package quizes.quiz1.makar_lavrov_1;
 
 /**
  * Correct formatting, naming conventions and the code itself
  * 1 point
  */
+
 public class Assignment01 {
     public static void main(String[] args) {
 
-        int A = 17;
-        int B = 18;
+        int a = 17; // new variable name
+        int b = 18; // new variable name
 
-        System.out.println("The sum of the numbers is" + A + B);
+        System.out.println("The sum of the numbers is " + (a + b)); // corrected sum operation
     }
 }
