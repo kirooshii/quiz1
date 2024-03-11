b8303c12e8f61aad0f80f2344eb9b7301d6a9962
Makar Lavrov <120464003+kirooshii@users.noreply.github.com>
2024-03-11 07:38:28 +0000

    solution 3

--- Assignment03.java
+++ Assignment03.java
@@ -7,5 +7,7 @@ package quizes.q1.makar_lavrov_1;
  * 0.5 points
  */
 public class Assignment03 {
-
+    public static void main(String[] args) {
+        System.out.println("The location of the hidden.txt file is: quizes/q1/makar_lavrov_1/coupe/jeans/cow/hidden.txt");
+    }
 }
